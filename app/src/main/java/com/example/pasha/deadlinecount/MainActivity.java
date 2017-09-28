cbec7c1cc13450c46c851e21e80ea06d77e952e7
Pasha <hrytsiuk27@gmail.com>
2017-09-28 23:50:01 +0300

    [add] count of hour logical adn display it

--- app/src/main/java/com/example/pasha/deadlinecount/MainActivity.java
+++ app/src/main/java/com/example/pasha/deadlinecount/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.pasha.deadlinecount;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.hoursView)
+    TextView hoursView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Calendar dateNow = new GregorianCalendar();
+        long timeNow = dateNow.getTimeInMillis();
+        Calendar dateDeadline = new GregorianCalendar(2018, 0, 1, 0, 0, 0);
+        long timeDeadline = dateDeadline.getTimeInMillis();
+        Long hours = ((timeDeadline - timeNow) / (3600 * 1000));
+
+        hoursView.setText(hours.toString());
     }
 }
